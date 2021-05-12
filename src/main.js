383d1a6bde63d368c015288af897a491bf920e01
DasomJung24 <jeongdasom6@gmail.com>
2021-05-13 00:08:11 +0900

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import store from "./store";
+import router from "./router";
+import VueRouter from "vue-router";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+Vue.use(VueRouter);
+
+Vue.config.productionTip = false;
 
 new Vue({
+  store,
+  router,
   render: h => h(App),
-}).$mount('#app')
+}).$mount("#app");
