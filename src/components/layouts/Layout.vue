7d79972e285538bc0f8d01acbbe7464a35c18981
DasomJung24 <jeongdasom6@gmail.com>
2021-05-15 14:23:12 +0900

    layouts

---
+++ src/components/layouts/Layout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="back-url"></div>
+    <Nav />
+  </div>
+</template>
+<script>
+export default {
+  name: "Layout",
+  data() {
+    return {};
+  },
+  components: {
+    Nav: () => import("@/components/layouts/Nav.vue"),
+  },
+};
+</script>
---
+++ src/components/layouts/Nav.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="nav space-between">
+    <div class="flex">
+      <div>
+        <span style="margin-right: 30px; color: grey;">VIP LOUNGE</span>
+        <span style="margin-right: 30px; color: grey;">멤버십</span>
+        <span style="color: grey;">고객센터</span>
+      </div>
+      <div class="space-between w-500">
+        <div>
+          <font-awesome-icon class="nav-icon" icon="bars" size="2x" />
+          <font-awesome-icon class="nav-icon" icon="search" size="2x" />
+        </div>
+        <div class="space-between w-300">
+          <span class="nav-text">영화</span>
+          <span class="nav-text">예매</span>
+          <span class="nav-text">극장</span>
+        </div>
+      </div>
+    </div>
+    <div class="nav-icon">
+      <img src="@/assets/logo-white_new.png" />
+    </div>
+    <div class="flex">
+      <div class="space-between">
+        <div></div>
+        <div>
+          <span style="color: grey;">로그인</span>
+          <span style="margin-left: 30px; color: grey;">회원가입</span>
+          <span style="margin-left: 30px; color: grey;">빠른예매</span>
+        </div>
+      </div>
+      <div class="space-between w-500">
+        <div class="space-between w-350">
+          <span class="nav-text">이벤트</span>
+          <span class="nav-text">스토어</span>
+          <span class="nav-text">혜택</span>
+        </div>
+        <div>
+          <font-awesome-icon class="nav-icon" icon="calendar-alt" size="2x" />
+          <font-awesome-icon class="nav-icon" icon="user" size="2x" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
