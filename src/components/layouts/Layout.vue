c134be7b624df0a7411a76dc556410d73b9ebf8e
DasomJung24 <jeongdasom6@gmail.com>
2021-05-18 00:17:24 +0900

    layout update

--- src/components/layouts/Layout.vue
+++ src/components/layouts/Layout.vue
@@ -1,7 +1,17 @@
 <template>
   <div>
-    <div class="back-url"></div>
-    <Nav />
+    <div class="back-url">
+      <div class="back-layer">
+        <Nav />
+        <div class="card-container">
+          <div class="box">
+            <p style="padding-bottom: 5px; ">박스오피스</p>
+            <div class="more-movie">더 많은 영화보기 +</div>
+          </div>
+          <MainMovie />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -12,6 +22,7 @@ export default {
   },
   components: {
     Nav: () => import("@/components/layouts/Nav.vue"),
+    MainMovie: () => import("@/components/cards/MainCard.vue"),
   },
 };
 </script>
