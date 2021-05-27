c7ed8d322afa841e9add2fad56b8570a3bfef63f
DasomJung24 <jeongdasom6@gmail.com>
2021-05-27 22:31:50 +0900

    layout

--- src/components/layouts/Layout.vue
+++ src/components/layouts/Layout.vue
@@ -1,37 +1,10 @@
 <template>
   <div>
-    <div class="back-url">
-      <div class="back-layer">
+    <div v-bind:class="{ backUrl: this.$route.name === 'MainList' }">
+      <div v-bind:class="{ backLayer: this.$route.name === 'MainList' }">
         <Nav />
-        <div class="card-container">
-          <div class="box">
-            <p style="padding-bottom: 5px; ">박스오피스</p>
-            <div class="more-movie">더 많은 영화보기 +</div>
-          </div>
-          <MainMovie />
-          <div class="bottom-bar">
-            <div>
-              <input
-                class="movie-input"
-                placeholder="영화명을 입력해주세요"
-              /><font-awesome-icon icon="search" size="lg" color="grey" />
-            </div>
-            <div>
-              <font-awesome-icon
-                :icon="['far', 'calendar-alt']"
-                size="lg"
-                color="grey"
-              /><span style="color: #fff; margin-left: 10px;">상영시간표</span>
-            </div>
-            <div>
-              <font-awesome-icon icon="film" size="lg" color="grey" />
-              <span style="color: #fff; margin-left: 10px;">박스오피스</span>
-            </div>
-            <div>
-              <font-awesome-icon icon="ticket-alt" size="lg" color="grey" />
-              <span style="color: #fff; margin-left: 10px;">빠른예매</span>
-            </div>
-          </div>
+        <div class="main-container">
+          <router-view />
         </div>
       </div>
     </div>
@@ -45,7 +18,6 @@ export default {
   },
   components: {
     Nav: () => import("@/components/layouts/Nav.vue"),
-    MainMovie: () => import("@/components/cards/MainCard.vue"),
   },
 };
 </script>
