df1835043ba919bbcea80ddb8d789c7b4050fca6
DasomJung24 <jeongdasom6@gmail.com>
2021-06-02 21:34:12 +0900

    nav

--- src/components/layouts/Nav.vue
+++ src/components/layouts/Nav.vue
@@ -1,44 +1,47 @@
 <template>
-  <div class="nav space-between">
-    <div class="flex">
-      <div>
-        <span style="margin-right: 30px; color: grey;">VIP LOUNGE</span>
-        <span style="margin-right: 30px; color: grey;">멤버십</span>
-        <span style="color: grey;">고객센터</span>
-      </div>
-      <div class="space-between w-500">
-        <div>
-          <font-awesome-icon class="nav-icon" icon="bars" size="2x" />
-          <font-awesome-icon class="nav-icon" icon="search" size="2x" />
+  <div class="nav">
+    <div class="nav-container space-between">
+      <div class="flex">
+        <div class="nav-top-l">
+          <span>VIP LOUNGE</span>
+          <span>멤버십</span>
+          <span>고객센터</span>
         </div>
-        <div class="space-between w-300">
-          <span class="nav-text">영화</span>
-          <span class="nav-text">예매</span>
-          <span class="nav-text">극장</span>
-        </div>
-      </div>
-    </div>
-    <div class="nav-icon">
-      <img src="@/assets/logo-white_new.png" />
-    </div>
-    <div class="flex">
-      <div class="space-between">
-        <div></div>
-        <div>
-          <span style="color: grey;">로그인</span>
-          <span style="margin-left: 30px; color: grey;">회원가입</span>
-          <span style="margin-left: 30px; color: grey;">빠른예매</span>
+        <div class="space-between white gnb">
+          <div style="margin-right: 50px;">
+            <font-awesome-icon class="nav-icon" icon="bars" size="lg" />
+            <font-awesome-icon icon="search" size="lg" />
+          </div>
+          <div class="space-between">
+            <span class="nav-text" @click="$router.push({ name: 'MovieList' })"
+              >영화</span
+            >
+            <span class="nav-text">예매</span>
+            <span class="nav-text">극장</span>
+          </div>
         </div>
       </div>
-      <div class="space-between w-500">
-        <div class="space-between w-350">
-          <span class="nav-text">이벤트</span>
-          <span class="nav-text">스토어</span>
-          <span class="nav-text">혜택</span>
-        </div>
+      <div class="nav-main">
+        <img src="@/assets/logo-white_new.png" />
+      </div>
+      <div class="flex">
         <div>
-          <font-awesome-icon class="nav-icon" icon="calendar-alt" size="2x" />
-          <font-awesome-icon class="nav-icon" icon="user" size="2x" />
+          <div class="nav-top-r">
+            <span>로그인</span>
+            <span>회원가입</span>
+            <span>빠른예매</span>
+          </div>
+        </div>
+        <div class="space-between white gnb">
+          <div class="space-between">
+            <span class="nav-text">이벤트</span>
+            <span class="nav-text">스토어</span>
+            <span class="nav-text">혜택</span>
+          </div>
+          <div style="margin-left: 50px;">
+            <font-awesome-icon class="nav-icon" icon="calendar-alt" size="lg" />
+            <font-awesome-icon icon="user" size="lg" />
+          </div>
         </div>
       </div>
     </div>
