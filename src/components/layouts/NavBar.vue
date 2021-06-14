e86f0dc964b0889e05fe32812aa704c98d9192be
DasomJung24 <jeongdasom6@gmail.com>
2021-06-14 10:38:53 +0900

    nav-bar

--- src/components/layouts/NavBar.vue
+++ src/components/layouts/NavBar.vue
@@ -16,13 +16,13 @@
       </div>
     </div>
     <div class="event-bar" v-if="data === 'event'">
-      <div v-for="movie in movies" :key="movie">
-        {{ movie }}
+      <div v-for="event in events" :key="event">
+        {{ event }}
       </div>
     </div>
     <div class="gift-bar" v-if="data === 'gift'">
-      <div v-for="movie in movies" :key="movie">
-        {{ movie }}
+      <div v-for="gift in gifts" :key="gift">
+        {{ gift }}
       </div>
     </div>
   </div>
@@ -31,14 +31,14 @@
 export default {
   name: 'NavBarComponent',
   props: { data: String },
-  data() {
+  data () {
     return {
       movies: ['전체영화', 'N스크린', '큐레이션', '무비포스트'],
       reservations: ['빠른예매', '상영시간표', '더 부티크 프라이빗 예매'],
       theaters: ['전체극장', '특별관'],
       events: ['진행중 이벤트', '지난 이벤트', '당첨자발표'],
-      gifts: ['메가박스 멤버십', '제휴/할인'],
-    };
-  },
-};
+      gifts: ['메가박스 멤버십', '제휴/할인']
+    }
+  }
+}
 </script>
